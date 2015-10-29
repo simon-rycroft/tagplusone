d0f1237dd7a760e3d01ba952495fb86de91ea87e
Simon Rycroft <simon.rycroft@subcode.io>
2015-10-29 06:33:19 +0000

    Trying to figure out why sync.calls is empty.

--- src/lib/GitClient.js
+++ src/lib/GitClient.js
@@ -4,19 +4,22 @@ import {spawn} from 'child_process';
 
 export default class GitClient {
 
-    tag() {
+    tag(done) {
         let tag = spawn('git', ['tag']);
 
         tag.stdout.on('data', (data) => {
             console.log('stdout: ' + data);
+            done(data);
         });
 
         tag.stderr.on('data', (data) => {
             console.log('stderr: ' + data);
+            done(data);
         });
 
         tag.on('close', (code) => {
             console.log('child process exited with code ' + code);
+            done(code);
         });
     }
 }
--- tests/src/unit/GitClientTest.js
+++ tests/src/unit/GitClientTest.js
@@ -2,41 +2,56 @@
 
 import {assert, expect} from 'chai';
 import mockery from 'mockery';
+import async from 'async';
 import mockSpawn from 'mock-spawn';
 import _ from 'lodash';
 
 import GitClient from '../../../src/lib/GitClient';
 
-describe('The Git Client', function() {
+describe('The Git Client', () => {
 
-    // TODO async
- 
     let git,
         spawn;
 
-    beforeEach(function() {
+    beforeEach(() => {
         git = new GitClient();
         spawn = mockSpawn(false);
         mockery.enable({ useCleanCache: true });
         mockery.registerMock('child_process', { spawn: spawn });
     });
 
-    describe('tag method', function() {
-
-        it('should exist', function() {
-            assert.isFunction(git.tag);
-        });
-
-        it('should execute the shell command "git tag"', function () {
-            git.tag();
-            assert.equal(1, spawn.calls.length, 'Incorrect number of command calls.')
-            assert.equal('tag', spawn.calls[0].command);
-        });
-    });
-
-    afterEach(function() {
+    afterEach(() => {
         mockery.deregisterAll();
         mockery.resetCache();
         mockery.disable();
+    });
+
+    describe('tag method', () => {
+
+        it('should exist', () => {
+            assert.isFunction(git.tag);
+        });
+
+        it('should execute the shell command "git tag"', (done) => {
+            spawn.setDefault(
+                spawn.simple(0)
+            );
+            git.tag((err) => {
+                assert.ok(!err, 'tag() threw an unexpected error');
+                console.log(spawn.calls);
+                assert.equal(1, spawn.calls.length, 'spawn() was not called the correct number of times');
+                assert.equal('git', spawn.call.command, 'incorrect spawn() command called');
+                done();
+            })
+            //async.series([
+            //    (next) => { git.tag(next); }
+            //], (err) => {
+            //    assert.ok(!err);
+            //    done();
+            //});
+            //git.tag();
+            //assert.equal(1, spawn.calls.length, 'Incorrect number of command calls.')
+            //assert.equal('tag', spawn.calls[0].command);
+        });
     });
 });
